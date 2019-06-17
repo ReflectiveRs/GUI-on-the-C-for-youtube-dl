46975698ac7175ffad7a80914677105d88993f94
Sergey <silver256r@gmail.com>
2019-06-17 19:23:06 +0300

    create

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(737, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Получить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(108, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(607, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(108, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(829, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(108, 157);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(829, 26);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(25, 529);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 56);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Скачать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.Color.Black;
+            this.tableLayoutPanel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(25, 311);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(912, 197);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Статус";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Код формата и ссылка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(595, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Путь сохранения";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(25, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(242, 60);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Добавить в список";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(21, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "AUDIO:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(21, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "VIDEO:";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(33, 616);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(48, 17);
+            this.labelStatus.TabIndex = 10;
+            this.labelStatus.Text = "Status";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(793, 224);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 33);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Выбрать путь";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(311, 224);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(461, 27);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(307, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Сохранить в:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(20, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 28);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "URL:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(983, 661);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelStatus;
+        public void Labs (string info)
+        {
+            labelStatus.Text = info;
+        }
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static Form1 instance = null; // For label down
+        InfoDownload infoDown = new InfoDownload(); // Класс InfoDownload хранит информацию для скачивания
+        SaveFile file = new SaveFile(); // Для сохранения в файл
+        Download download = new Download(); // Для загрузки
+
+        public Form1()
+        {
+            InitializeComponent();
+            instance = this;
+            checkSaveFile(); // Проверить остались ли файлы для скачивания
+        }
+        
+
+        // Проверка на незавершенные сохранения и их добавления в таблицу
+        public void checkSaveFile()
+        {
+            String[] saveDown = file.read();
+
+            // Если файл невозможно прочитать 
+            if (saveDown == null)
+            {
+                return;
+            }
+
+            // Заполнить таблицу нескаченными видео
+            for (int i =0;i<saveDown.Length-1;i++)
+            {
+                String[] param = saveDown[i].Split('|');
+                tableLayoutPanel1.Controls.Add(new Label() { Text = "none" });
+                tableLayoutPanel1.Controls.Add(new TextBox() { Text = param[0], Size = new System.Drawing.Size(350, 20), ReadOnly = true });
+                tableLayoutPanel1.Controls.Add(new TextBox() { Text = param[1], Size = new System.Drawing.Size(200, 20), ReadOnly = true });
+            }
+        }
+
+        // Получение параметров для скачивания
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string result = await Task.Factory.StartNew<string>(
+                                             () => Getlink(textBox1.Text),
+                                             TaskCreationOptions.LongRunning);
+            // Очищаем comboBox от записей
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+
+            if (result != "")
+            {
+                string[] line = result.Split('\n');
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    // Только аудио
+                    if (line[i].Contains("audio only"))
+                    {
+                        line[i] = line[i].Replace("audio only DASH audio ", "");
+                        line[i] = line[i].Replace("    ", " ");
+                        comboBox1.Items.Add(line[i]);
+                    }
+                    // Только видео
+                    else if (line[i].Contains("video only"))
+                    {
+                        line[i] = line[i].Replace("    ", " ");
+                        comboBox2.Items.Add(line[i]);
+                    }
+                }
+                infoDown.Url = textBox1.Text;
+            }
+            else
+            {
+                MessageBox.Show("Ссылки не получены");
+            } 
+        }
+
+        /*async void RunAsyncGetLink(string str)
+        {
+            Console.WriteLine(str);
+            await Task.Run(() => Getlink(str));
+        }*/
+
+        string Getlink(string str)
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "youtube-dl.exe",
+                    Arguments = "-F " + str,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            string allLink = "";
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine(); // вывод из youtube-dl
+                int temp;
+
+                // Найдем строки необходимые для загрузки (Добавляем строки с первым числовым значением)
+                if (Int32.TryParse(line.Split(' ')[0], out temp))
+                {
+                    allLink += line + "\n";
+                }
+
+            }
+            MessageBox.Show("Готово");
+            return allLink;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; ; i++)
+            {
+                // Если значение пустое то остановить
+                if (tableLayoutPanel1.GetControlFromPosition(0, i) == null)
+                {
+                    return;
+                }
+                // Если файл скачен тогда продолить
+                if (tableLayoutPanel1.GetControlFromPosition(0, i).Text == "Скачено")
+                {
+                    continue;
+                }
+                tableLayoutPanel1.GetControlFromPosition(0, i).Text = "В процессе";
+
+                // Запустить закачку
+                var stat = await Task.Run(() => download.Run(tableLayoutPanel1.GetControlFromPosition(1, i).Text + " -o \"" + tableLayoutPanel1.GetControlFromPosition(2, i).Text+ "\\\\%(title)s-%(id)s.%(ext)s\""));
+                
+                if (stat == "ERROR")
+                {
+                    tableLayoutPanel1.GetControlFromPosition(0, i).Text = "Ошибка";
+                }
+                else
+                {
+                    tableLayoutPanel1.GetControlFromPosition(0, i).Text = "Скачено";
+                }
+                // Удалить запись с файла
+                file.delete(tableLayoutPanel1.GetControlFromPosition(1, i).Text + "|" + tableLayoutPanel1.GetControlFromPosition(2, i).Text+";");
+            }
+        }
+
+        // Добавить в таблицу
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string kod_format = "";
+
+            if (comboBox2.Text != "")
+            {
+                kod_format += comboBox2.Text.Split(' ')[0] + "+";
+            }
+
+            if (comboBox1.Text != "")
+            {
+                kod_format += comboBox1.Text.Split(' ')[0];
+            }
+            else
+            {
+                MessageBox.Show("Не выбрано аудио");
+                return;
+            }
+            infoDown.FormatCode = kod_format;
+
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "none" });
+            tableLayoutPanel1.Controls.Add(new TextBox() { Text = kod_format + " " + infoDown.Url, Size = new Size(350, 20), ReadOnly = true });
+            tableLayoutPanel1.Controls.Add(new TextBox() { Text = infoDown.Path, Size = new Size(200, 20), ReadOnly = true });
+
+            // Добавить в сохр. файл
+            file.write(kod_format + " " + infoDown.Url + "|" + infoDown.Path);
+        }
+
+        // Показать статус загрузки
+        public static void Status(string info)
+        {
+            instance.StatusWrite(info);
+        }
+        public void StatusWrite(string info)
+        {
+            labelStatus.Invoke(new Action(() => labelStatus.Text = info));
+        }
+
+        // Выбрать путь для сохранения
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string folderName = folderBrowserDialog1.SelectedPath;
+                textBox2.Text = folderName;
+                infoDown.Path = folderName;
+            }
+        }
+    }
+    class Download
+    {
+        private bool status = false; // Статус загруки
+        private Process proc = new Process();
+        public String Run(string kod_url) // Отслеживание статуса загруки
+        {
+            if (status == true)
+            {
+                return ""; // Возвратить если закрука активна
+            }
+
+            status = true;
+            String stat = startDownload(kod_url);
+            status = false;
+            return stat;
+        }
+
+        public String startDownload(string kod_url)
+        {
+            proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "youtube-dl.exe",
+                    Arguments = "-f" + kod_url,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+
+            string lineError = null;
+
+            // вывод из youtube-dl
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine(); 
+                Form1.Status(line);   
+            }
+
+            // ERROR от youtube-dl
+            lineError = proc.StandardError.ReadLine();
+            if (lineError != null)
+            {
+                if (lineError.Contains("ERROR"))
+                {
+                    MessageBox.Show(lineError);
+                    return "ERROR";
+                }
+                lineError = null;
+            }
+            Form1.Status("Всё скачено");
+            return "OK";
+        }
+    }
+
+    // Работа с файлом сохранения
+    class SaveFile
+    {
+        public String line;
+
+        public String[] read()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("save.txt"))
+                {
+                    line = sr.ReadToEnd();
+                    String[] urlSave = line.Split(';');
+                    return urlSave;
+                }
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("[ERROR] Файл с сохранениями не прочитан:");
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public void delete(String url)
+        {
+            line = line.Replace(url, "");
+            System.IO.File.WriteAllText("save.txt", line);
+        }
+
+        public void write(string save)
+        {
+            line += save+";";
+            System.IO.File.WriteAllText("save.txt", line);
+        }
+    }
+
+    // Информация о скачивающимся видео 
+    class InfoDownload
+    {
+        private string url = "";
+        private string formatCode = "";
+        private string path = "";
+
+        public string Url
+        {
+            get
+            {
+                return url;
+            }
+            set
+            {
+                url = value;
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                  return path;
+            }
+            set
+            {
+                path = value;
+            }
+        }
+
+        public string FormatCode 
+        {
+            get
+            {
+                return formatCode;
+            }
+            set
+            {
+                formatCode = value;
+            }
+        }
+    }
+}
